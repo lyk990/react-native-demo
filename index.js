e114d89ee52964361e4007b64b9a5ce494a0613b
lyk990 <1477435414@qq.com>
2023-07-03 17:52:32 +0800

    test app name

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /**
  * @format
+ * @description: 入口文件
  */
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+// rn根节点的初始化
 AppRegistry.registerComponent(appName, () => App);
